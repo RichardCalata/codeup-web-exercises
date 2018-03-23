81d56c8334807c0708506755740c50343570171b
richardcalata <calatar6@gmail.com>
2018-03-22 19:00:12 -0500

    loop exercises

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,20 +1,43 @@
 "use strict";
 
 
+
+
+
+for(var i = 0; i < 10; i++){
+    i = i.toString();
+    console.log(i.repeat(i));
+}
+
+
+//
+// function isEven(i){
+//
+//     return number % 2 == 0
+// }
+
+
 var number = prompt("an ODD number between 1 and 50");
 
-for(var i=0; i<0;i++){
+for(var i=1; i<51;i++){
 
-    console.log(i);
-
-    if(i === number){
-
-        continue;
-    }
-    if(isNaN(i)){
-document.write("That's not a number!");
+    if(isNaN(number)){
+    document.write("That's not a number!");
         break;
 
     }
+    if(i % 2 == 0) {
+
+        continue;
+    }
+    if(i == number){
+        console.log("ALERT!! SKIPPING "  + i)
+        continue;
+    }
+
+
+    console.log(i);
+    document.write(i);
+
 
 }
