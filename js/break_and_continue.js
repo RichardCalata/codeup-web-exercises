2acceb855c0af75e27eacd1cd4214085e7b45430
richardcalata <calatar6@gmail.com>
2018-03-23 10:27:25 -0500

    am added break at prompt "not a number"

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -14,11 +14,24 @@
 var number = prompt("an ODD number between 1 and 50");
     console.log("We will be skipping " + number + ".");
 
+
+// function isEven(number) {
+//
+//     return number % 2 == 0;
+// }
+
+while(isNaN(parseFloat(number))){
+
+
+    number = prompt("an ODD number between 1 and 50");
+}
+
 for(var i=1; i<51;i++){
 
 
     if(isNaN(number)){
     document.write("That's not a number!");
+
         break;
 
     }
@@ -37,3 +50,5 @@ for(var i=1; i<51;i++){
 
 
 }
+
+
