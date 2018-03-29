700a3b1c48362a3c33f7dede7e464be19b9bf956
richardcalata <calatar6@gmail.com>
2018-03-29 13:46:37 -0500

    Defuse the BOM

---
+++ js/testing.js
@@ -0,0 +1,28 @@
+"use strict";
+
+
+
+// var a = Math.floor(Math.random()*100);
+// var b = Math.floor(Math.random()*100);
+// var c = Math.floor(Math.random()*100);
+// var d = Math.floor(Math.random()*100);
+// var e = Math.floor(Math.random()*100);
+/**************************************************/
+// var n = [a, b, c, d, e];
+//
+// n.forEach(function(number){
+//     number = Math.floor(Math.random()*100);
+// });
+//
+//     console.log("Smallest Integer is " + Math.min.apply(this,n));
+
+/***************************************************/
+    // var minInt = n.reduce(function(a,b){
+    //     return Math.min(a,b);
+    // });
+
+// function sumTwoSmallestNumbers(n) {
+//
+// };
+//
+// sumTwoSmallestNumbers(n);
