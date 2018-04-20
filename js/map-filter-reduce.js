442f01ce15011009ad420d0844e388ee117882ef
richardcalata <calatar6@gmail.com>
2018-04-20 11:32:52 -0500

    for each to for of

--- js/map-filter-reduce.js
+++ js/map-filter-reduce.js
@@ -36,23 +36,37 @@ const has3 = users
 //     .map(user => user.name);
     // .filter(name =>  name.languages)
 
-    const emai = users
-.map(email=>email.email);
+//     const emai = users
+// .map(email=>email.email);
+//
+//     const reduction = users
+//         .map(user => users.id)
+//     .reduce((reduction, userId)=>{
+//         return reduction.push(userId)
+//
+//     },{});
 
-    const reduction = users
-        .map(user => users.id)
-    .reduce((reduction, userId)=>{
-        return reduction.concat(userId)
-
-    },[]);
 const numbers = [1, 2, 3, 4, 5];
 
 const sum = numbers.reduce((accumulation, currentNumber) => {
     return accumulation + currentNumber;
 }, 0);
 
+// const result ={
+//     "1": {
+//
+//         id: 5,
+//         name: 'justin',
+//         email: 'justin@codeup.com',
+//         languages: ['html', 'css', 'javascript', 'php'],
+//     },
+//     }
+// }
 
-
+const result =
+users.reduce((accumulator,user)=>{
+    return accumulator[users.id] = user;
+},{});
 //     1 Use .filter to create an array of user objects where each user object has at least 3 languages in the languages array.
 //     2 Use .map to create an array of strings where each element is a user's email address
 //     3 Use .reduce to transform the array into an object where the object's keys are ids and the values are objects that represent each user
