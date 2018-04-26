463f5692c2dc08cc9be7005738013293ec65374e
richardcalata <calatar6@gmail.com>
2018-04-26 08:34:53 -0500

    promises

---
+++ js/promises.js
@@ -0,0 +1,19 @@
+
+const wait = milliseconds =>{
+    setTimeout(() => {
+        return new Promise(f(r,r)=>{
+            resolve();
+        };
+    }, milliseconds);
+};
+
+wait(1000).then(()=>{});
+
+
+const wait = milliseconds => {
+    return new Promise((resolve,reject) =>{
+        setTimeOut(() =>{
+            resolve();
+        }, milliseconds);
+    });
+};
